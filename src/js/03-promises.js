874b32701a55d702a1d326cc1292a7d73d59cc46
MartaSyvyk <shavaliuk.marta@gmail.com>
2022-11-01 22:51:12 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,59 +1,59 @@
-function createPromise(position, delay) {
+// function createPromise(position, delay) {
 
-  const shouldResolve = Math.random() > 0.3;
+//   const shouldResolve = Math.random() > 0.3;
 
-  const promise = newPromise ((resolve, reject) => {
+//   const promise = newPromise ((resolve, reject) => {
   
-  setTimeout( () => {
-      if (shouldResolve) {
+//   setTimeout( () => {
+//       if (shouldResolve) {
    
-    resolve("Success");
+//     resolve("Success");
   
 
-  } else {
-    reject("Error");
-    // Reject
-  }}, delay);
-  return promise;
-}
-};
-
-
-  function onPromiseSuccess (result) {
-    console.log(result, position);
-  }
-
-  function onPromiseFail (error, position) {
-    console.log(error);
-  }
-
-const p = createPromise(2, 500);
-
+//   } else {
+//     reject("Error");
+//     // Reject
+//   }}, delay);
+//   return promise;
+// }
+// };
 
 
 //   function onPromiseSuccess (result) {
-//     console.log(result);
+//     console.log(result, position);
 //   }
 
-//   function onPromiseFail (error) {
+//   function onPromiseFail (error, position) {
 //     console.log(error);
 //   }
 
-//   createPromise(position, delay) {
-
-//   }
+// const p = createPromise(2, 500);
 
 
-// let PromptDelay = 1000;
-// let DelayStep = 500;
-// let PromptCounter = 0;
-// let maxAttempts = 4;
 
-// const intervalId = setInterval(() => {
-//   if(PromptCounter === maxAttempts) {
-//     console.log("done");
-//     return;
-//   }
-//   PromptCounter += 1;
-//   createPromise()
-// })
+// //   function onPromiseSuccess (result) {
+// //     console.log(result);
+// //   }
+
+// //   function onPromiseFail (error) {
+// //     console.log(error);
+// //   }
+
+// //   createPromise(position, delay) {
+
+// //   }
+
+
+// // let PromptDelay = 1000;
+// // let DelayStep = 500;
+// // let PromptCounter = 0;
+// // let maxAttempts = 4;
+
+// // const intervalId = setInterval(() => {
+// //   if(PromptCounter === maxAttempts) {
+// //     console.log("done");
+// //     return;
+// //   }
+// //   PromptCounter += 1;
+// //   createPromise()
+// // })
