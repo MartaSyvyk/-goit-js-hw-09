9d6b4de93b3cf8d24d2c73a6f03588ce319d4630
MartaSyvyk <shavaliuk.marta@gmail.com>
2022-11-01 22:48:51 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,59 @@
 function createPromise(position, delay) {
+
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+
+  const promise = newPromise ((resolve, reject) => {
+  
+  setTimeout( () => {
+      if (shouldResolve) {
+   
+    resolve("Success");
+  
+
   } else {
+    reject("Error");
     // Reject
-  }
+  }}, delay);
+  return promise;
 }
+};
+
+
+  function onPromiseSuccess (result) {
+    console.log(result, position);
+  }
+
+  function onPromiseFail (error, position) {
+    console.log(error);
+  }
+
+const p = createPromise(2, 500);
+
+
+
+//   function onPromiseSuccess (result) {
+//     console.log(result);
+//   }
+
+//   function onPromiseFail (error) {
+//     console.log(error);
+//   }
+
+//   createPromise(position, delay) {
+
+//   }
+
+
+// let PromptDelay = 1000;
+// let DelayStep = 500;
+// let PromptCounter = 0;
+// let maxAttempts = 4;
+
+// const intervalId = setInterval(() => {
+//   if(PromptCounter === maxAttempts) {
+//     console.log("done");
+//     return;
+//   }
+//   PromptCounter += 1;
+//   createPromise()
+// })
